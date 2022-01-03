a4ceddc3c97d05d44032e32ebc60fe8cd0c5f0da
Y K <ultraman0713@gmail.com>
2022-01-03 17:32:40 +0900

    typo

--- js/revMain.js
+++ js/revMain.js
@@ -70,93 +70,93 @@ const btn5 = document.querySelector('#l_half_daytime');
 const btn6 = document.querySelector('#evening');
 const btn7 = document.querySelector('#nighttime');
 btn1.addEventListener('click', () => {
-	document.body.style.backgroundImage = "url('./img/dawn.webp')";
+    document.body.style.backgroundImage = "url('./img/dawn.webp')";
 });
 btn2.addEventListener('click', () => {
-	document.body.style.backgroundImage = "url('./img/f_half_morning.webp')";
+    document.body.style.backgroundImage = "url('./img/f_half_morning.webp')";
 });
 btn3.addEventListener('click', () => {
-	document.body.style.backgroundImage = "url('./img/l_half_morning.webp')";
+    document.body.style.backgroundImage = "url('./img/l_half_morning.webp')";
 });
 btn4.addEventListener('click', () => {
-	document.body.style.backgroundImage = "url('./img/f_half_daytime.webp')";
+    document.body.style.backgroundImage = "url('./img/f_half_daytime.webp')";
 });
 btn5.addEventListener('click', () => {
-	document.body.style.backgroundImage = "url('./img/l_half_daytime.webp')";
+    document.body.style.backgroundImage = "url('./img/l_half_daytime.webp')";
 });
 btn6.addEventListener('click', () => {
-	document.body.style.backgroundImage = "url('./img/evening.webp')";
+    document.body.style.backgroundImage = "url('./img/evening.webp')";
 });
 btn7.addEventListener('click', () => {
-	document.body.style.backgroundImage = "url('./img/nighttime.webp')";
+    document.body.style.backgroundImage = "url('./img/nighttime.webp')";
 });
 
 //normal sequence shows up below.
 // const writeText = document.getElementById('title');
 const writeText = document.getElementById('greeting_words');
 const changeText = function() {
-	if (ct >= dawn && ct < f_half_morning) {
-		writeText.innerHTML = 'Morning, dawn chages everything.';
-	} else {
-		if (ct >= f_half_morning && ct < l_half_morning) {
-			writeText.innerHTML = 'Good morning.';
-		} else {
-			if (ct >= l_half_morning && ct < f_half_daytime) {
-				writeText.innerHTML = 'Wish you a good day.';
-			} else {
-				if (ct >= f_half_daytime && ct < f_half_daytime) {
-					writeText.innerHTML = 'Good afternoon.';
-				} else {
-					if (ct >= f_half_daytime && ct < l_half_daytime) {
-						writeText.innerHTML = 'Wish you a blissful afternoon.';
-					} else {
-						if (ct >= evening && ct < nighttime) {
-							writeText.innerHTML = 'Good evening.';
-						} else {
-							writeText.innerHTML = 'Wish you a relaxing time.';
-						}
-					}
-				}
-			}
-		}
-	}
+    if (ct >= dawn && ct < f_half_morning) {
+        writeText.innerHTML = 'Morning, dawn changes everything.';
+    } else {
+        if (ct >= f_half_morning && ct < l_half_morning) {
+            writeText.innerHTML = 'Good morning.';
+        } else {
+            if (ct >= l_half_morning && ct < f_half_daytime) {
+                writeText.innerHTML = 'Wish you a good day.';
+            } else {
+                if (ct >= f_half_daytime && ct < f_half_daytime) {
+                    writeText.innerHTML = 'Good afternoon.';
+                } else {
+                    if (ct >= f_half_daytime && ct < l_half_daytime) {
+                        writeText.innerHTML = 'Wish you a blissful afternoon.';
+                    } else {
+                        if (ct >= evening && ct < nighttime) {
+                            writeText.innerHTML = 'Good evening.';
+                        } else {
+                            writeText.innerHTML = 'Wish you a relaxing time.';
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 const changeBackgroundImage = function() {
-	if (ct >= dawn && ct < f_half_morning) {
-		document.body.style.backgroundImage = "url('./img/dawn.webp')";
-	} else {
-		if (ct >= f_half_morning && ct < l_half_morning) {
-			document.body.style.backgroundImage = "url('./img/f_half_morning.webp')";
-		} else {
-			if (ct >= l_half_morning && ct < f_half_daytime) {
-				document.body.style.backgroundImage = "url('./img/l_half_morning.webp')";
-			} else {
-				if (ct >= f_half_daytime && ct < l_half_daytime) {
-					document.body.style.backgroundImage = "url('./img/f_half_daytime.webp')";
-				} else {
-					if (ct >= l_half_daytime && ct < evening) {
-						document.body.style.backgroundImage = "url('./img/l_half_daytime.webp')";
-					} else {
-						if (ct >= evening && ct < nighttime) {
-							document.body.style.backgroundImage = "url('./img/evening.webp')";
-						} else {
-							document.body.style.backgroundImage = "url('./img/nighttime.webp')";
-						}
-					}
-				}
-			}
-		}
-	}
+    if (ct >= dawn && ct < f_half_morning) {
+        document.body.style.backgroundImage = "url('./img/dawn.webp')";
+    } else {
+        if (ct >= f_half_morning && ct < l_half_morning) {
+            document.body.style.backgroundImage = "url('./img/f_half_morning.webp')";
+        } else {
+            if (ct >= l_half_morning && ct < f_half_daytime) {
+                document.body.style.backgroundImage = "url('./img/l_half_morning.webp')";
+            } else {
+                if (ct >= f_half_daytime && ct < l_half_daytime) {
+                    document.body.style.backgroundImage = "url('./img/f_half_daytime.webp')";
+                } else {
+                    if (ct >= l_half_daytime && ct < evening) {
+                        document.body.style.backgroundImage = "url('./img/l_half_daytime.webp')";
+                    } else {
+                        if (ct >= evening && ct < nighttime) {
+                            document.body.style.backgroundImage = "url('./img/evening.webp')";
+                        } else {
+                            document.body.style.backgroundImage = "url('./img/nighttime.webp')";
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 //for storing visited times on this site in their own localStorage in their browsers.
 const counter = function() {
-	if (!localStorage['times']) {
-		localStorage['times'] = 0;
-	}
-	let cnt = localStorage['times'];
-	document.getElementById('cntr_text').innerHTML = 'Thank you for the ' + ++localStorage['times'] + ' times visiting.';
+    if (!localStorage['times']) {
+        localStorage['times'] = 0;
+    }
+    let cnt = localStorage['times'];
+    document.getElementById('cntr_text').innerHTML = 'Thank you for the ' + ++localStorage['times'] + ' times visiting.';
 }
 
 changeText();
