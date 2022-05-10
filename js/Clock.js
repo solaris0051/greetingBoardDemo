ec2a5cdb220741b873f2fd0f334715cff223a9b5
Y K <ultraman0713@gmail.com>
2022-05-11 06:21:19 +0900

    unifiefd variable of current time to ct.

--- js/Clock.js
+++ js/Clock.js
@@ -1,16 +1,14 @@
-onmessage = function (event) {
-  let param = event.data;
-  if (param == `request`) {
-    let numTimer = setTimeout(function perMinute() {
-      let curTime = new Date();
-      let hour = Math.floor(
-        (curTime % (60 * 60 * 60 * 1000)) / (60 * 60 * 1000)
-      );
-      let min = Math.floor((curTime % (60 * 60 * 1000)) / (60 * 1000));
-      let sec = Math.floor((curTime % (60 * 1000)) / 1000);
-      console.log(`Clock`, `|`, hour, `:`, min, `:`, sec);
-      postMessage([hour, min, sec]);
-      numTimer = setTimeout(perMinute, 1000);
-    }, 1000);
-  }
+onmessage = function(event) {
+	let param = event.data;
+	if (param == `request`) {
+		let numTimer = setTimeout(function perMinute() {
+			ct = new Date();
+			let hour = Math.floor((ct % (60 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+			let min = Math.floor((ct % (60 * 60 * 1000)) / (60 * 1000));
+			let sec = Math.floor((ct % (60 * 1000)) / 1000);
+			console.log(`Clock`, `|`, hour, `:`, min, `:`, sec);
+			postMessage([hour, min, sec]);
+			numTimer = setTimeout(perMinute, 1000);
+		}, 1000);
+	}
 };
