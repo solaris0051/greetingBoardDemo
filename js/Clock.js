b1393b0ea9c8acd37ee8831fd9256cbd379cf3af
Y K <joshua0051.tree@outlook.com>
2022-05-15 08:24:18 +0900

    hour corrected.

--- js/Clock.js
+++ js/Clock.js
@@ -2,7 +2,7 @@ onmessage = function(event) {
 	if (event.data == 1) {
 		let numTimer = setTimeout(function perSecond() {
 			let w_ct = new Date();
-			let hour = Math.floor((w_ct % (60 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+			let hour = Math.floor((w_ct % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
 			let min = Math.floor((w_ct % (60 * 60 * 1000)) / (60 * 1000));
 			let sec = Math.floor((w_ct % (60 * 1000)) / 1000);
 			console.log(`Clock`, `|`, hour, `:`, min, `:`, sec);
