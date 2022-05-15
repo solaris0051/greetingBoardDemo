3ecce5dd1d98166926cb78947f35fb89fb76ae1e
Y K <joshua0051.tree@outlook.com>
2022-05-15 17:17:54 +0900

    Plus unsaved lines in Clock.js.

--- js/Clock.js
+++ js/Clock.js
@@ -3,9 +3,9 @@ onmessage = function(event) {
 		let numTimer = setTimeout(function perQMin() {
 			let w_ct = new Date();
 			w_ct.getHours;
-			console.log(`viaClock: before clock postMessage: ${w_ct}`);
+			console.log(`viaClock.js: before clock postMessage: ${w_ct}`);
 			postMessage(w_ct);
-			console.log(`viaClock: after clock postMessage: ${w_ct}`);
+			console.log(`viaClock.js: after clock postMessage: ${w_ct}`);
 			numTimer = setTimeout(perQMin, 15 * 1000);
 		}, 15 * 1000);
 	}
