c563f1ea428629d64bce0e3c82eef5ad7e9acd7f
Y K <docodoco.0713@outlook.com>
2022-07-28 22:24:26 +0900

    Update Clock.js

--- js/Clock.js
+++ js/Clock.js
@@ -3,9 +3,7 @@ onmessage = function(event) {
 		let numTimer = setTimeout(function perQMin() {
 			let cto = new Date();
 			cto.getHours;
-			console.log(`viaClock.js: before clock postMessage: ${cto}`);
 			postMessage(cto);
-			console.log(`viaClock.js: after clock postMessage: ${cto}`);
 			numTimer = setTimeout(perQMin, 15 * 1000);
 		}, 15 * 1000);
 	}
