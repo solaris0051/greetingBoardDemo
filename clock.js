7c5ebbe53fddb4b642156ef93833d90ceded3ba2
Y K <docodoco.0713@outlook.com>
2022-10-29 09:06:38 +0900

    css chore & setInterval applied to clock.js

--- clock.js
+++ clock.js
@@ -1,10 +1,9 @@
 onmessage = function (event) {
   if (event.data === `move`) {
-    let timerID0 = setTimeout(function fifteenseconds() {
+    setInterval(() => {
       let cto = new Date();
       cto.getHours;
       postMessage(cto);
-      timerID0 = setTimeout(fifteenseconds, 15 * 1000);
     }, 15 * 1000);
   }
 };
