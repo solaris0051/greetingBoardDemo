d62abd499a18def5763cd566df3097af08e0053a
Y K <docodoco.0713@outlook.com>
2023-01-26 17:23:00 +0900

    [string].slice(-2)

--- time.js
+++ time.js
@@ -2,9 +2,9 @@ addEventListener("message", (event) => {
     if (event.data === `request`) {
         setInterval(() => {
             let currentTime = new Date();
-            let hrs = currentTime.getHours();
-            let mins = currentTime.getMinutes();
-            let secs = currentTime.getSeconds();
+            let hrs = `${0}${currentTime.getHours()}`.slice(-2);
+            let mins = `${0}${currentTime.getMinutes()}`.slice(-2);
+            let secs = `${0}${currentTime.getSeconds()}`.slice(-2);
             postMessage(hrs + `:` + mins + `:` + secs);
         }, 100);
     }
