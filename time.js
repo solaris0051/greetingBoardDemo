6765eedfb61ed43a473a3de04943d0544c1ac0f0
Y K <docodoco.0713@outlook.com>
2023-01-27 19:29:42 +0900

    forge!

--- time.js
+++ time.js
@@ -2,10 +2,13 @@ addEventListener("message", (event) => {
     if (event.data === `request`) {
         setInterval(() => {
             let currentTime = new Date();
-            let hrs = `${0}${currentTime.getHours()}`.slice(-2);
-            let mins = `${0}${currentTime.getMinutes()}`.slice(-2);
-            let secs = `${0}${currentTime.getSeconds()}`.slice(-2);
-            postMessage(hrs + `:` + mins + `:` + secs);
+            let yy = `${currentTime.getFullYear()}`;
+            let mo = `${currentTime.getMonth() + 1}`;
+            let dd = `${0}${currentTime.getDate()}`.slice(-2);
+            let hh = `${0}${currentTime.getHours()}`.slice(-2);
+            let mm = `${0}${currentTime.getMinutes()}`.slice(-2);
+            let ss = `${0}${currentTime.getSeconds()}`.slice(-2);
+            postMessage(`${yy}年${mo}月${dd}日 ${hh}:${mm}:${ss}`);
         }, 100);
     }
 });
