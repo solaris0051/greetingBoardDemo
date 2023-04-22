dfcfce1382664b87f0dad009eeca3b04286e87f3
Y K <docodoco.0713@outlook.com>
2023-04-22 18:54:33 +0900

    tersed

--- time.js
+++ time.js
@@ -1,14 +1,1 @@
-addEventListener("message", (event) => {
-    if (event.data === `request`) {
-        setInterval(() => {
-            let currentTime = new Date();
-            let yy = `${currentTime.getFullYear()}`;
-            let mo = `${currentTime.getMonth() + 1}`;
-            let dd = `${0}${currentTime.getDate()}`.slice(-2);
-            let hh = `${0}${currentTime.getHours()}`.slice(-2);
-            let mm = `${0}${currentTime.getMinutes()}`.slice(-2);
-            let ss = `${0}${currentTime.getSeconds()}`.slice(-2);
-            postMessage(`${yy}年${mo}月${dd}日 ${hh}:${mm}:${ss}`);
-        }, 100);
-    }
-});
+addEventListener("message",(e=>{"request"===e.data&&setInterval((()=>{let e=new Date,t=`${e.getFullYear()}`,s=`${e.getMonth()+1}`,$=`0${e.getDate()}`.slice(-2),a=`0${e.getHours()}`.slice(-2),g=`0${e.getMinutes()}`.slice(-2),l=`0${e.getSeconds()}`.slice(-2);postMessage(`${t}年${s}月${$}日 ${a}:${g}:${l}`)}),100)}));
