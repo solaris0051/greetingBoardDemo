647c60b8ac2a02a7672096f8d6c456b27a797b62
Y K <docodoco.0713@outlook.com>
2023-05-30 21:39:30 +0900

    revert & -

--- src/clock.js
+++ src/clock.js
@@ -2,8 +2,7 @@ addEventListener("message", (event) => {
     if (event.data === `move`) {
         setInterval(() => {
             let cto = new Date();
-            let currentTime = cto.getHours;
-            postMessage(currentTime);
+            postMessage(cto);
         }, 15 * 1000);
     };
 })
